00545bea9ea3dbfa5ba994a5a0005b3137c9e6a9
EnokhIovan <jacobjovan26@gmail.com>
2026-08-04 14:26:15 +0700

    Adding homescreen music and dead SFX, adjust scripts

--- Colorless_files/Scenes/Code/MusicPlayer.gd
+++ Colorless_files/Scenes/Code/MusicPlayer.gd
@@ -1,9 +1,10 @@
 extends Node
 
 const TRACKS := {
-	"main": "res://Colorless_files/Sound/Pijar Kelabu/Pijar Kelabu BGM.wav",
-	"raining": "res://Colorless_files/Sound/Pijar Kelabu/Pijar Kelabu BGM - Raining.wav",
-	"harmoni": "res://Colorless_files/Sound/Pijar Kelabu/Pijar Kelabu - Harmoni.wav",
+	"main": "res://Colorless_files/Sound/Musik/Pijar Kelabu/Pijar Kelabu BGM.wav",
+	"raining": "res://Colorless_files/Sound/Musik/Pijar Kelabu/Pijar Kelabu BGM - Raining.wav",
+	"harmoni": "res://Colorless_files/Sound/Musik/Pijar Kelabu/Pijar Kelabu - Harmoni.wav",
+	"homescreen": "res://Colorless_files/Sound/Musik/Melodi Chromata.wav",
 }
 const FADE_TIME := 1.5
 
