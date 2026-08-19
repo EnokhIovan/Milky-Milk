00ae46c8924bd1289df941d8ae7d3a014681c8ed
EnokhIovan <jacobjovan26@gmail.com>
2026-08-19 22:06:55 +0700

    Fixing Shard.gd

--- Colorless_files/Scenes/Code/InteractiveSystem/Shard.gd
+++ Colorless_files/Scenes/Code/InteractiveSystem/Shard.gd
@@ -2,34 +2,29 @@ extends Area2D
 
 @export var id: String = ""
 
-var player_inside: Node = null
+var player_inside := false
 
 func check_shard_color():
 	$AnimatedSprite2D.play(id)
 
+func _on_body_entered(body: CharacterBody2D):
+	if body:
+		body.interaction_prompt.visible = true
+		player_inside = true
+
+func _on_body_exited(body: CharacterBody2D):
+	if body.is_in_group("player"):
+		body.interaction_prompt.visible = false
+		player_inside = false
+
 func _physics_process(_delta: float) -> void:
-	var bodies := get_overlapping_bodies()
-	
 	check_shard_color()
 	
 	if SaveManager.is_shard_picked(id):
 		queue_free()
-
-	player_inside = null
-
-	for body in bodies:
-		if body.is_in_group("player"):
-			player_inside = body
-			break
-
-	# Update prompt
-	if player_inside:
-		player_inside.interaction_prompt.visible = true
-
-		if Input.is_action_just_pressed("interact"):
-			player_inside.interaction_prompt.visible = false
+	
+	if player_inside == true and Input.is_action_just_pressed("interact"):
+			print("yay")
+			get_tree().current_scene.get_node("Player/InteractionPrompt").visible = false
 			SaveManager.pick_shard(id)
 			queue_free()
-	else:
-		for body in get_tree().get_nodes_in_group("player"):
-			body.interaction_prompt.visible = false
